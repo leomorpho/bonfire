9186b9e625a145b8871bc95f5a382aa20e76087d
Leo Audibert <leonard.audibert@gmail.com>
2024-10-27 09:22:24 -0700

    Update thought list to use Card

--- src/routes/(user)/dashboard/+page.svelte
+++ src/routes/(user)/dashboard/+page.svelte
@@ -2,7 +2,7 @@
 	import { page } from '$app/stores';
 	import { writable } from 'svelte/store';
 	import { Button } from '$lib/components/ui/button';
-
+	import * as Card from '$lib/components/ui/card/index.js';
 	const thoughtsList = writable($page.data.thoughts);
 </script>
 
@@ -17,12 +17,37 @@
 		{#if $thoughtsList.length > 0}
 			<div class="space-y-2">
 				{#each $thoughtsList as thought}
-					<div class="rounded-lg border border-gray-300 p-4 shadow-sm">
-						<p class="text-gray-800">{thought.thought}</p>
-						<p class="text-sm text-gray-500">
-							Created at: {new Date(thought.createdAt).toLocaleString()}
-						</p>
-					</div>
+					<Card.Root class="w-full">
+						<Card.Header>
+							<Card.Title>{thought.thought}</Card.Title>
+							<Card.Description
+								>Created at: {new Date(thought.createdAt).toLocaleString()}</Card.Description
+							>
+						</Card.Header>
+						<Card.Content>
+							<!-- Add info on work progress on this thought -->
+						</Card.Content>
+						<Card.Footer class="flex justify-between">
+							<Button variant="outline"
+								><svg
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									class="lucide lucide-ellipsis w-4 h-4"
+									><circle cx="12" cy="12" r="1" /><circle cx="19" cy="12" r="1" /><circle
+										cx="5"
+										cy="12"
+										r="1"
+									/></svg
+								></Button
+							>
+							<Button>Work</Button>
+						</Card.Footer>
+					</Card.Root>
 				{/each}
 			</div>
 		{:else}
