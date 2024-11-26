d94a894d34bddfc97019628d5a11ceef47b5da65
Leo Audibert <leonard.audibert@gmail.com>
2024-11-26 13:42:53 -0800

    Add alert for file deletion

--- src/routes/(bonfire)/bonfire/[[id]]/media/gallery/+page.svelte
+++ src/routes/(bonfire)/bonfire/[[id]]/media/gallery/+page.svelte
@@ -9,11 +9,12 @@
 	import { Toggle } from '$lib/components/ui/toggle/index.js';
 	import PhotoSwipeLightbox from 'photoswipe/lightbox';
 	import 'photoswipe/style.css';
-	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
 	import { ImagePlus } from 'lucide-svelte';
 	import * as ContextMenu from '$lib/components/ui/context-menu/index.js';
 	import { SquareMousePointer } from 'lucide-svelte';
 	import * as Tooltip from '$lib/components/ui/tooltip/index.js';
+	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.ts';
 
 	let selectedImages: any = $state([]);
 	let selection: any;
@@ -362,15 +363,32 @@
 			<Tooltip.Provider>
 				<Tooltip.Root>
 					<Tooltip.Trigger>
-						<button
-							disabled={selectedImages.length == 0}
-							onclick={handleDelete}
-							class="rounded-full p-4 text-white shadow-lg transition
-							{selectedImages.length === 0 ? 'cursor-not-allowed bg-red-100' : 'bg-red-500 hover:bg-red-600'}"
-						>
-							<!-- Button Icon -->
-							<Trash2 class="h-6 w-6" />
-						</button></Tooltip.Trigger
+						<AlertDialog.Root>
+							<AlertDialog.Trigger >
+								<button
+								disabled={selectedImages.length == 0}
+								onclick={handleDelete}
+								class="rounded-full p-4 text-white shadow-lg transition
+								{selectedImages.length === 0 ? 'cursor-not-allowed bg-red-100' : 'bg-red-500 hover:bg-red-600'}"
+							>
+								<!-- Button Icon -->
+								<Trash2 class="h-6 w-6" />
+							</button>
+							</AlertDialog.Trigger>
+							<AlertDialog.Content>
+							  <AlertDialog.Header>
+								<AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+								<AlertDialog.Description>
+								  This action cannot be undone. This will permanently delete these files from our servers.
+								</AlertDialog.Description>
+							  </AlertDialog.Header>
+							  <AlertDialog.Footer>
+								<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+								<AlertDialog.Action class="bg-red-500 hover:bg-red-400">Continue</AlertDialog.Action>
+							  </AlertDialog.Footer>
+							</AlertDialog.Content>
+						  </AlertDialog.Root>
+						</Tooltip.Trigger
 					>
 					<Tooltip.Content>
 						<p>Delete</p>
