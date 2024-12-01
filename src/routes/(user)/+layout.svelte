6035bdf699339b48923d681786a19b054a2efdc2
Leo Audibert <leonard.audibert@gmail.com>
2024-12-01 14:53:02 -0800

    Redirect to username page if not set (from be)

--- src/routes/(user)/+layout.svelte
+++ src/routes/(user)/+layout.svelte
@@ -1,30 +1,8 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
-	import { getFeTriplitClient, userIdStore } from '$lib/triplit';
-	import type { TriplitClient } from '@triplit/client';
-	import { onMount } from 'svelte';
+	import { userIdStore } from '$lib/triplit';
 
 	userIdStore.set($page.data.user.id);
-
-	let client: TriplitClient;
-
-	onMount(() => {
-		client = getFeTriplitClient($page.data.jwt) as TriplitClient;
-		const init = async () => {
-			const query = client.query('user').where('id', '=', $page.data.user.id).build();
-			let result = await client.fetch(query);
-
-			if (result.length == 0) {
-				return;
-			}
-			if (result[0].username.length == 0) {
-				goto('/profile/username');
-			}
-		};
-
-		init();
-	});
 </script>
 
 <slot />
