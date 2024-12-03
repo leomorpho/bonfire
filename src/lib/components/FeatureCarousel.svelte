cc916eae99db5d28e76225d0950762256bf1696e
Leo Audibert <leonard.audibert@gmail.com>
2024-12-02 16:26:15 -0800

    Flesh out landing page

--- src/lib/components/FeatureCarousel.svelte
+++ src/lib/components/FeatureCarousel.svelte
@@ -3,7 +3,19 @@
 	import type { EmblaCarouselType, EmblaOptionsType } from 'embla-carousel';
 	import emblaCarouselSvelte from 'embla-carousel-svelte';
 
-	import { Aperture, Dice6, Drama, Factory, Smile } from 'lucide-svelte';
+	import {
+		Aperture,
+		BellRing,
+		Dice6,
+		Drama,
+		Factory,
+		Flame,
+		Images,
+		Link,
+		Music,
+		Smile,
+		Users
+	} from 'lucide-svelte';
 	import Container from './Container.svelte';
 
 	let emblaApi: EmblaCarouselType;
@@ -35,66 +47,76 @@
 
 <Container>
 	<div class="py-24 md:py-32">
-		<h2 class="font-bold text-2xl md:text-4xl mb-16 text-center">
+		<h2 class="mb-16 text-center text-2xl font-bold md:text-4xl">
 			Everything you need to fix your problem right now
 		</h2>
 		<!-- on mobile show small icons without text -->
-		<div class="w-fit mx-auto mb-5 sm:hidden">
+		<div class="mx-auto mb-5 w-fit sm:hidden">
 			<div class="join">
-				<button class:btn-primary={index === 0} class="join-item btn" onclick={() => setIndex(0)}
-					><Smile />
+				<button class:btn-primary={index === 0} class="btn join-item" onclick={() => setIndex(0)}
+					><Flame class="h-4 w-4" />
 				</button>
-				<button class:btn-primary={index === 1} class="join-item btn" onclick={() => setIndex(1)}
-					><Dice6 /></button
+				<button class:btn-primary={index === 1} class="btn join-item" onclick={() => setIndex(1)}
+					><Link class="h-4 w-4" /></button
 				>
-				<button class:btn-primary={index === 2} class="join-item btn" onclick={() => setIndex(2)}
-					><Aperture /></button
+				<button class:btn-primary={index === 2} class="btn join-item" onclick={() => setIndex(2)}
+					><Users class="h-4 w-4" /></button
 				>
-				<button class:btn-primary={index === 3} class="join-item btn" onclick={() => setIndex(3)}
-					><Drama /></button
+				<button class:btn-primary={index === 3} class="btn join-item" onclick={() => setIndex(3)}
+					><Music class="h-4 w-4" /></button
 				>
-				<button class:btn-primary={index === 4} class="join-item btn" onclick={() => setIndex(4)}
-					><Factory /></button
+				<button class:btn-primary={index === 4} class="btn join-item" onclick={() => setIndex(4)}
+					><Images class="h-4 w-4" /></button
+				>
+				<button class:btn-primary={index === 5} class="btn join-item" onclick={() => setIndex(5)}
+					><BellRing class="h-4 w-4" /></button
 				>
 			</div>
 		</div>
 
 		<!-- on desktop show large icons with text -->
-		<div class="hidden w-fit mx-auto space-x-10 sm:flex">
+		<div class="mx-auto hidden w-fit space-x-10 sm:flex">
 			<button
 				class:text-primary={index === 0}
 				class="flex flex-col items-center"
 				onclick={() => setIndex(0)}
-				><Smile class="w-10 h-10" />
-				<div class="text-xl font-semibold mt-2">Smile</div>
+				><Flame class="h-10 w-10" />
+				<div class="mt-2 text-xl font-semibold">Plan</div>
 			</button>
 			<button
 				class:text-primary={index === 1}
 				class="flex flex-col items-center"
 				onclick={() => setIndex(1)}
-				><Dice6 class="w-10 h-10" />
-				<div class="text-xl font-semibold mt-2">Dice</div></button
+				><Link class="h-10 w-10" />
+				<div class="mt-2 text-xl font-semibold">Share</div></button
 			>
 			<button
 				class:text-primary={index === 2}
 				class="flex flex-col items-center"
 				onclick={() => setIndex(2)}
-				><Aperture class="w-10 h-10" />
-				<div class="text-xl font-semibold mt-2">Aperture</div></button
+				><Users class="h-10 w-10" />
+				<div class="mt-2 text-xl font-semibold">Attendees</div></button
 			>
 			<button
 				class:text-primary={index === 3}
 				class="flex flex-col items-center"
 				onclick={() => setIndex(3)}
-				><Drama class="w-10 h-10" />
-				<div class="text-xl font-semibold mt-2">Drama</div></button
+				><Music class="h-10 w-10" />
+				<div class="mt-2 text-xl font-semibold">Playlists</div></button
 			>
 			<button
 				class:text-primary={index === 4}
 				class="flex flex-col items-center"
 				onclick={() => setIndex(4)}
-				><Factory class="w-10 h-10" />
-				<div class="text-xl font-bold mt-2">Factory</div></button
+				><Images class="h-10 w-10" />
+				<div class="mt-2 text-xl font-bold">Memories</div></button
+			>
+			<button
+				class:text-primary={index === 5}
+				class="flex flex-col items-center"
+				onclick={() => setIndex(5)}
+				><BellRing class="h-10 w-10" />
+				<div class="mt-2 text-xl font-bold">Auto-reminders</div></button
 			>
 		</div>
 		<div class="embla pt-14">
@@ -106,37 +128,75 @@
 				<div class="embla__container">
 					<div class="embla__slide">
 						<div
-							class="rounded-xl flex items-center justify-center text-xl font-bold h-80 bg-slate-100"
+							class="flex h-80 flex-col items-center justify-center space-y-3 rounded-xl bg-slate-100 p-5 text-center"
 						>
-							<span>feature 1</span>
+							<div class="flex items-center sm:text-xl font-bold">
+								<Flame class="hidden sm:block mr-1 h-4 w-4 fill-pink-700 sm:h-5 sm:w-5" />Create a bonfire in
+								seconds
+							</div>
+							<p class="text-xs sm:text-sm md:text-base">Creating an event is quick and simple.</p>
+							<p class="text-xs sm:text-sm md:text-base">You can select from our many fun themes or upload your own image.</p>
 						</div>
 					</div>
 					<div class="embla__slide">
 						<div
-							class="rounded-xl flex items-center justify-center text-xl font-bold h-80 bg-slate-100"
+							class="flex h-80 flex-col items-center justify-center space-y-3 rounded-xl bg-slate-100 p-5 text-center"
 						>
-							<span>feature 2</span>
+							<div class="flex items-center sm:text-xl font-bold">
+								<Link class="hidden sm:block mr-1 h-4 w-4 sm:h-6 sm:w-6" />Quick-share URLs
+							</div>
+							<p class="text-xs sm:text-sm md:text-base">Each Bonfire has a unique URL that can be shared anywhere beyond the app.</p>
+							<p class="text-xs sm:text-sm md:text-base">
+								Keep it private by sharing it to your close circle, or watch it grow like wildfire
+								by sharing publicly on social media.
+							</p>
 						</div>
 					</div>
 					<div class="embla__slide">
 						<div
-							class="rounded-xl flex items-center justify-center text-xl font-bold h-80 bg-slate-100"
+							class="flex h-80 flex-col items-center justify-center space-y-3 rounded-xl bg-slate-100 p-5 text-center"
 						>
-							<span>feature 3</span>
+							<div class="flex items-center sm:text-xl font-bold">
+								<Users class="hidden sm:block mr-1 h-4 w-4 fill-yellow-500 sm:h-6 sm:w-6" />Track & Notify Attendees
+							</div>
+							<p class="text-xs sm:text-sm md:text-base">Track who's coming and keep the hype going by posting updates and content.</p>
+							<p class="text-xs sm:text-sm md:text-base">RSVP'd users can upload their own images of the event to the group gallery.</p>
 						</div>
 					</div>
 					<div class="embla__slide">
 						<div
-							class="rounded-xl flex items-center justify-center text-xl font-bold h-80 bg-slate-100"
+							class="flex h-80 flex-col items-center justify-center space-y-3 rounded-xl bg-slate-100 p-5 text-center"
 						>
-							<span>feature 4</span>
+							<div class="flex items-center sm:text-xl font-bold">
+								<Music class="hidden sm:block mr-1 h-4 w-4 sm:h-6 sm:w-6" />Shared Playlists
+							</div>
+							<p class="text-xs sm:text-sm md:text-base">Attach a Spotify playlist link to share a group playlist for your Bonfire.</p>
 						</div>
 					</div>
 					<div class="embla__slide">
 						<div
-							class="rounded-xl flex items-center justify-center text-xl font-bold h-80 bg-slate-100"
+							class="flex h-80 flex-col items-center justify-center space-y-3 rounded-xl bg-slate-100 p-5 text-center"
 						>
-							<span>feature 5</span>
+							<div class="flex items-center sm:text-xl font-bold">
+								<Images class="hidden sm:block mr-1 h-4 w-4 sm:h-6 sm:w-6 " />Log Memories
+							</div>
+							<p class="text-xs sm:text-sm md:text-base">
+								Store all the images from everyone at the event in one place and generate a
+								highlight reel of the best moments from the event
+							</p>
+						</div>
+					</div>
+					<div class="embla__slide">
+						<div
+							class="flex h-80 flex-col items-center justify-center space-y-3 rounded-xl bg-slate-100 p-5 text-center"
+						>
+							<div class="flex items-center sm:text-xl font-bold">
+								<BellRing class="hidden sm:block mr-1 h-4 w-4 fill-green-500 sm:h-6 sm:w-6" />Auto-Reminders
+							</div>
+							<p class="text-xs sm:text-sm md:text-base">
+								Set automatic reminders for attendees to ensure they don't forget about upcoming
+								events.
+							</p>
 						</div>
 					</div>
 				</div>
--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,31 +1,31 @@
 <script>
 	import { Ship } from 'lucide-svelte';
 	import Container from './Container.svelte';
+	import Button from './ui/button/button.svelte';
 </script>
 
 <!-- The exact line breaks and widths of various sections should depend on the copy. Make sure the copy looks good on
 all screen sizes (not the case here) -->
 <Container>
-	<div class="flex flex-col md:flex-row py-8 sm:py-20">
+	<div class="flex flex-col py-8 sm:py-20 md:flex-row">
 		<div>
-			<div class="text-center text-4xl sm:text-7xl sm:text-left font-bold sm:mt-8">
-				You put your apps <br /> <span class="text-secondary">main promise</span> <br /> right
-				<span class="underline decoration-primary">here</span>
+			<div class="text-center text-4xl font-bold sm:mt-8 sm:text-left sm:text-7xl">
+				Re-ignite your <br /> <span class="text-pink-500">social life</span> <br /> right
+				<span class="underline decoration-purple-500">here</span>
 			</div>
-			<p class="mt-2 sm:mt-4 mb-6 sm:mb-8 text-md text-center sm:text-left sm:text-lg">
-				You can put some subtext here about with a further explanation <br
-					class="hidden sm:block"
-				/> of what features your app has to offer.
+			<p class="text-md mb-6 mt-2 text-center sm:mb-8 sm:mt-4 sm:text-left sm:text-lg">
+				Host and share memorable events with your friends.
 			</p>
 			<div class="flex items-center justify-center sm:justify-start">
-				<a href="/" class="btn btn-primary text-lg w-full max-w-sm h-14">
-					<Ship class="mr-2" /> Your call to action
-				</a>
+				<a href="/" > 
+					<Button class="h-14 text-lg bg-purple-500">Start a Bonfire</Button>
+					 </a>
+				<div class="italic mx-3">it's free!</div>
 			</div>
 		</div>
-		<div class=" bg-gray-100 md:ml-5 p-10 flex-1 text-xl rounded-xl mt-8 text-center">
-			You can put lots of things in this section. A quick demo video of your product, a carousel of
-			images, some testimonials or relevant logos. Be creative!
+		<div class=" mt-8 flex-1 rounded-xl bg-gray-100 p-10 text-center text-xl md:ml-5">
+			Put a quick demo video of the product, a carousel of
+			images, some testimonials or relevant logos. 
 		</div>
 	</div>
 </Container>
