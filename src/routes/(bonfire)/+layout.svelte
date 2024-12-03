73209302e49cced4c3c1d13a569b96f258397f55
Leo Audibert <leonard.audibert@gmail.com>
2024-12-03 14:33:06 -0800

    Lay groundwork for bonfire theme

--- src/routes/(bonfire)/+layout.svelte
+++ src/routes/(bonfire)/+layout.svelte
@@ -1,28 +1,14 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { userIdStore } from '$lib/triplit';
+	import type { Snippet } from 'svelte';
+	import type { LayoutData } from './$types';
+
+	let { data, children }: { data: LayoutData; children: Snippet } = $props();
 
 	if ($page.data.user) {
 		userIdStore.set($page.data.user.id);
 	}
 </script>
 
-<div class="bg-color min-h-screen w-full">
-	<slot />
-</div>
-
-<style>
-	.bg-color {
-		--s: 75px; /* control the size*/
-		--c1: #c02942;
-		--c2: #53777a;
-		--c3: #ecd078;
-		--c4: #d95b43;
-
-		background:
-			radial-gradient(var(--c1) 24%, #0000 25%),
-			radial-gradient(var(--c2) 30%, #0000 32%) calc(var(--s) / 2) calc(var(--s) / 2),
-			repeating-conic-gradient(from 30deg, var(--c3) 0 30deg, var(--c4) 0 25%);
-		background-size: var(--s) var(--s);
-	}
-</style>
+{@render children()}
---
+++ src/routes/(bonfire)/bonfire/[[id]]/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { LayoutData } from './$types';
+
+	let { data, children }: { data: LayoutData; children: Snippet } = $props();
+</script>
+
+<div class="bg-color min-h-screen w-full">
+	{@render children()}
+</div>
+
+<style>
+	.bg-color {
+		--s: 75px; /* control the size*/
+		--c1: #c02942;
+		--c2: #53777a;
+		--c3: #ecd078;
+		--c4: #d95b43;
+
+		background:
+			radial-gradient(var(--c1) 24%, #0000 25%),
+			radial-gradient(var(--c2) 30%, #0000 32%) calc(var(--s) / 2) calc(var(--s) / 2),
+			repeating-conic-gradient(from 30deg, var(--c3) 0 30deg, var(--c4) 0 25%);
+		background-size: var(--s) var(--s);
+	}
+</style>
