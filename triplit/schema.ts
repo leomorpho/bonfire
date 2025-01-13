77c7129c032c19d96c01d1793db41895472a986b
Leo Audibert <leonard.audibert@gmail.com>
2025-01-13 13:48:40 -0800

    Make is_linked_file optional

--- triplit/schema.ts
+++ triplit/schema.ts
@@ -311,7 +311,7 @@ export const schema = {
 			// Link to supporting files; for example, for videos, we save a frame and link it to the video.
 			linked_file_id: S.Optional(S.String({ nullable: true, default: null, optional: true })),
 			linked_file: S.Optional(S.RelationById('files', '$linked_file_id')),
-			is_linked_file: S.Boolean({ default: false })
+			is_linked_file: S.Optional(S.Boolean({ default: false }))
 		}),
 		permissions: {
 			user: {
