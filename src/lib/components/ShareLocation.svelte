947d2bb311851170136b275fb332439770824744
Leo Audibert <leonard.audibert@gmail.com>
2025-01-16 14:42:15 -0800

    Remove console log

---
+++ src/lib/components/ShareLocation.svelte
@@ -0,0 +1,15 @@
+<script>
+	let { lat, lon } = $props();
+
+	function generateMapUrls(lat, lon) {
+		return {
+			googleMaps: `https://www.google.com/maps?q=${lat},${lon}`,
+			appleMaps: `https://maps.apple.com/?ll=${lat},${lon}`,
+			waze: `https://www.waze.com/ul?ll=${lat},${lon}&navigate=yes`,
+			openStreetMap: `https://www.openstreetmap.org/?mlat=${lat}&mlon=${lon}&zoom=16`,
+			bingMaps: `https://www.bing.com/maps?cp=${lat}~${lon}`
+		};
+	}
+
+    const urls = generateMapUrls(location);
+</script>
