7e8b72ae103ac62c5838c742edd7c8a050552d8f
Leo Audibert <leonard.audibert@gmail.com>
2025-01-28 09:21:54 -0800

    Resize all vid/images to have same size in gallery preview

--- src/lib/components/GalleryItem.svelte
+++ src/lib/components/GalleryItem.svelte
@@ -3,7 +3,16 @@
 	import { blurhashToImageCssString } from '@unpic/placeholder';
 	import { Play } from 'lucide-svelte';
 
-	let { url, wPixel, hPixel, fileName, urlActive=false, blurhash, fileType, preview = null } = $props();
+	let {
+		url,
+		wPixel,
+		hPixel,
+		fileName,
+		urlActive = false,
+		blurhash,
+		fileType,
+		preview = null
+	} = $props();
 
 	// TODO: only load image when it crosses screen
 	// TODO: blurhash not working for now, not sure why
@@ -14,56 +23,62 @@
 	const isVideo = fileType.startsWith('video/');
 </script>
 
-<a 
+<a
 	href={url}
 	class={`gallery-item ${urlActive ? 'disabled-link' : ''}`}
 	data-pswp-width={wPixel}
 	data-pswp-height={hPixel}
 	data-pswp-is-video={isVideo}
 >
-	{#if fileType.startsWith('image/')}
-		<Image
-			width={wPixel}
-			class="rounded-lg"
-			src={url}
-			layout="constrained"
-			aspectRatio={5 / 3}
-			alt={fileName}
-			background={placeholder}
-		/>
-	{:else if isVideo}
-		{#if preview}
-			<!-- Show preview for videos if available -->
-			<div class="relative rounded-lg object-cover">
-				<img class="rounded-lg" src={preview.URL} alt={fileName} width={wPixel} height={hPixel} />
+	<div class="relative aspect-[5/3] w-full overflow-hidden rounded-lg bg-gray-200">
+		{#if fileType.startsWith('image/')}
+			<Image
+				width={wPixel}
+				class="h-full w-full object-cover"
+				src={url}
+				layout="constrained"
+				aspectRatio={5 / 3}
+				alt={fileName}
+				background={placeholder}
+			/>
+		{:else if isVideo}
+			{#if preview}
+				<!-- Show preview for videos if available -->
+				<img
+					class="h-full w-full object-cover"
+					src={preview.URL}
+					alt={fileName}
+					width={wPixel}
+					height={hPixel}
+				/>
 				<!-- Play icon in the center of the preview image -->
 				<div class="absolute inset-0 flex items-center justify-center">
 					<Play class="h-10 w-10 text-white sm:h-16 sm:w-16" />
 				</div>
-			</div>
-			<video
-				class="hidden rounded-lg"
-				src={url}
-				poster={placeholder}
-				controls
-				width={wPixel}
-				height={hPixel}
-				>{fileName}<track kind="captions" />
-				Your browser does not support the video tag.
-			</video>
-		{:else}
-			<video
-				class="rounded-lg"
-				src={url}
-				poster={placeholder}
-				controls
-				width={wPixel}
-				height={hPixel}
-				>{fileName}<track kind="captions" />
-				Your browser does not support the video tag.
-			</video>
+				<video
+					class="hidden rounded-lg"
+					src={url}
+					poster={placeholder}
+					controls
+					width={wPixel}
+					height={hPixel}
+					>{fileName}<track kind="captions" />
+					Your browser does not support the video tag.
+				</video>
+			{:else}
+				<video
+					class="absolute inset-0 h-full w-full object-cover"
+					src={url}
+					poster={placeholder}
+					controls
+					width={wPixel}
+					height={hPixel}
+					>{fileName}<track kind="captions" />
+					Your browser does not support the video tag.
+				</video>
+			{/if}
 		{/if}
-	{/if}
+	</div>
 </a>
 
 <style>
