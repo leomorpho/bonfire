193a866ecf476bc6fe065e18341e4c4a86a89535
Leo Audibert <leonard.audibert@gmail.com>
2025-01-30 11:50:13 -0800

    Dont init turso in build

--- src/lib/server/database/db.ts
+++ src/lib/server/database/db.ts
@@ -1,17 +1,19 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { dev } from '$app/environment';
 import { createClient } from '@libsql/client';
-import { env as privateEnv} from '$env/dynamic/private';
-import * as schema from '$lib/server/database/schema'
+import { env as privateEnv } from '$env/dynamic/private';
+import * as schema from '$lib/server/database/schema';
 
-const url = dev ? 'file:local.db' : privateEnv.TURSO_DB_URL;
+function createDatabaseConnection() {
+	// 🔹 Only fetch env vars at runtime
+	const url = dev ? 'file:local.db' : privateEnv.TURSO_DB_URL;
+	const authToken = privateEnv.TURSO_DB_AUTH_TOKEN;
 
-if (!url) {
-	throw new Error('TURSO_DB_URL is not set');
+	if (!url) throw new Error('TURSO_DB_URL is not set');
+	if (!dev && !authToken) throw new Error('TURSO_DB_AUTH_TOKEN is not set');
+
+	const libsql = createClient({ url, authToken });
+	return drizzle(libsql, { schema });
 }
-if (!dev && !privateEnv.TURSO_DB_AUTH_TOKEN) {
-	throw new Error('TURSO_DB_AUTH_TOKEN is not set');
-}
 
-const libsql = createClient({ url, authToken: privateEnv.TURSO_DB_AUTH_TOKEN });
-export const db = drizzle(libsql, { schema });
+export const db = createDatabaseConnection();
