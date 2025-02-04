f91d346b7c86a8998b375e83c60e0ee8a0bf301b
Leo Audibert <leonard.audibert@gmail.com>
2025-02-04 10:00:30 -0800

    Update +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,24 +1,21 @@
 <script>
-	import Container from '$lib/components/Container.svelte';
-	import { page } from '$app/stores';
+	export let error;
 </script>
 
-<Container>
-	<main class="grid min-h-full place-items-center px-6 py-24 sm:py-32">
-		<h1 class="text-error text-4xl font-bold text-center">
-			{$page.status}
-		</h1>
-		<div>
-			<p class="text-center text-base-content/80 text-lg mt-4">
-				{#if $page.status === 404}
-					The page you are looking for does not exist.
-				{:else}
-					An error occurred.
-				{/if}
-			</p>
-			<p class="text-center mt-10">
-				<a href="/" class="btn">Go back home</a>
-			</p>
-		</div>
-	</main>
-</Container>
+<div class="grid min-h-full place-items-center px-6 py-24 sm:py-32">
+	<h1 class="text-error text-4xl font-bold text-center">
+		{error?.status || 500}
+	</h1>
+
+	<p class="text-center text-base-content/80 text-lg mt-4">
+		{#if error?.status === 404}
+			The page you are looking for does not exist.
+		{:else}
+			An unexpected error occurred.
+		{/if}
+	</p>
+
+	<p class="text-center mt-10">
+		<a href="/" class="btn">Go back home</a>
+	</p>
+</div>
