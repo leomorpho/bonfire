92cbe176f844076947a87834b8cff50a433bafc9
Leo Audibert <leonard.audibert@gmail.com>
2025-02-25 14:28:31 -0800

    Fix names in emoji context menu

--- src/lib/components/im/EmojiContextMenu.svelte
+++ src/lib/components/im/EmojiContextMenu.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import * as Popover from '$lib/components/ui/popover/index.js';
 	import ProfileAvatar from '../ProfileAvatar.svelte';
+	import EmojiContextMenuEntry from './EmojiContextMenuEntry.svelte';
 
 	let { children, toggleEmoji, reactions, currUserId } = $props();
 
@@ -24,23 +25,12 @@
 	</Popover.Trigger>
 	<Popover.Content class="w-fit rounded-xl bg-slate-300 p-1 text-sm dark:bg-slate-900">
 		{#each reactions as reaction}
-			<div
-				class="{reaction.user_id == currUserId
-					? 'hover:rounded-lg hover:bg-slate-400 dark:hover:bg-slate-800'
-					: ''} flex items-center justify-between p-2"
-			>
-				<span class="mr-2"><ProfileAvatar userId={reaction.user_id} baseHeightPx={25} /></span>
-				{#if reaction.user_id == currUserId}
-					<button onclick={removeEmoji}>
-						<div>{reaction.user_id}</div>
-						<div class="text-xs">Click to remove</div>
-					</button>
-					{@render emojiDef(reaction.emoji)}
-				{:else}
-					{reaction.user_id}
-					{@render emojiDef(reaction.emoji)}
-				{/if}
-			</div>
+			<EmojiContextMenuEntry
+				{currUserId}
+				{toggleEmoji}
+				reactionUserId={reaction.user_id}
+				reactionEmoji={reaction.emoji}
+			/>
 		{/each}
 	</Popover.Content>
 </Popover.Root>
---
+++ src/lib/components/im/EmojiContextMenuEntry.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import ProfileAvatar from '../ProfileAvatar.svelte';
+	import { userIdsStore, usersLiveDataStore } from '$lib/profilestore';
+
+	let { currUserId, toggleEmoji, reactionUserId, reactionEmoji } = $props();
+
+	let isOpen = $state(false);
+
+	const removeEmoji = async () => {
+		toggleEmoji();
+		isOpen = false;
+	};
+
+	let unsubscribe: any; // Store unsubscribe function
+	let username: string | null | undefined = $state('');
+
+	onMount(() => {
+		userIdsStore.update((ids) => [...new Set([...ids, reactionUserId])]);
+
+		unsubscribe = usersLiveDataStore.subscribe((users) => {
+			const user = users.get(reactionUserId);
+
+			username = user?.username;
+		});
+	});
+</script>
+
+{#snippet emojiDef(emoji: string)}
+	<span class="ml-2 text-2xl">
+		{emoji}
+	</span>
+{/snippet}
+
+<div
+	class="{reactionUserId == currUserId
+		? 'hover:rounded-lg hover:bg-slate-400 dark:hover:bg-slate-800'
+		: ''} flex items-center justify-between p-2"
+>
+	<span class="mr-2"><ProfileAvatar userId={reactionUserId} baseHeightPx={25} /></span>
+	{#if reactionUserId == currUserId}
+		<button onclick={removeEmoji}>
+			<div>{username}</div>
+			<div class="text-xs">Click to remove</div>
+		</button>
+		{@render emojiDef(reactionEmoji)}
+	{:else}
+		{username}
+		{@render emojiDef(reactionEmoji)}
+	{/if}
+</div>
