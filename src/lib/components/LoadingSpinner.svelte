23f8c0f29380b59de4add1692107e3f9aa7a9e23
Leo Audibert <leonard.audibert@gmail.com>
2025-03-04 16:02:23 -0800

    Show loader when going to stripe

--- src/lib/components/LoadingSpinner.svelte
+++ src/lib/components/LoadingSpinner.svelte
@@ -5,8 +5,18 @@
 <span class={`loader flex items-center justify-center ${cls}`}></span>
 
 <style>
+	:root {
+		--loader-color: #000; /* Default: Black for light mode */
+	}
+
+	@media (prefers-color-scheme: dark) {
+		:root {
+			--loader-color: #fff; /* White for dark mode */
+		}
+	}
+
 	.loader {
-		border: 5px solid #fff;
+		border: 5px solid var(--loader-color);
 		border-bottom-color: transparent;
 		border-radius: 50%;
 		display: inline-block;
