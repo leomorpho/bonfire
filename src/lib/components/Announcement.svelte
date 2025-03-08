ce9a56900a61d8113e68cc3d70aebc21ffb5eaa5
Leo Audibert <leonard.audibert@gmail.com>
2025-03-08 11:22:24 -0800

    Improve announcement design

--- src/lib/components/Announcement.svelte
+++ src/lib/components/Announcement.svelte
@@ -67,8 +67,8 @@
 </script>
 
 <Card.Root
-	class="pb-2 relative announcement border-0 bg-opacity-90 dark:bg-opacity-90 {announcement.seen_by.length == 0 &&
-	!isUnverifiedUser
+	class="announcement relative border-0 bg-opacity-90 pb-2 dark:bg-opacity-90 {announcement.seen_by
+		.length == 0 && !isUnverifiedUser
 		? 'bg-yellow-200 dark:bg-yellow-700'
 		: 'bg-slate-200 dark:bg-slate-800'} dark:text-white"
 >
@@ -76,7 +76,7 @@
 	{#if currUserId == announcement.user_id || isCurrenUserEventAdmin}
 		<a
 			href={`/bonfire/${eventId}/announcement/${announcement.id}/update`}
-			class="update-announcement absolute top-2 right-2"
+			class="update-announcement absolute right-2 top-2"
 		>
 			<Button class="rounded-xl p-2" variant="outline">
 				<Pencil class="!h-4 !w-4" />
@@ -84,17 +84,19 @@
 		</a>
 	{/if}
 
-	<Card.Header >
+	<Card.Header>
 		<Card.Title class="flex font-normal">
-			<div class="mr-2">
-				<ProfileAvatar userId={announcement.user_id} baseHeightPx={30} />
+			<div bind:this={cardRef} class="flex items-center justify-center w-full text-sm">
+				<div class="mr-2">
+					<ProfileAvatar userId={announcement.user_id} baseHeightPx={30} />
+				</div>
+				<div>
+					{formatHumanReadable(announcement.created_at)}
+				</div>
 			</div>
-			{announcement.content}
 		</Card.Title>
-		<Card.Description >
-			<div bind:this={cardRef}>
-				{formatHumanReadable(announcement.created_at)}
-			</div>
+		<Card.Description class="pb-2">
+			{announcement.content}
 		</Card.Description>
 	</Card.Header>
 </Card.Root>
